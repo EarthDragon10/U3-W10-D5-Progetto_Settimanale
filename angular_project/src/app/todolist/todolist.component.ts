43f7b95df252b297b0e97b98f10ecf37a7def8eb
EarthDragon10 <lagudadev@gmail.com>
2022-11-06 10:29:32 +0100

    fix link active

--- angular_project/src/app/todolist/todolist.component.ts
+++ angular_project/src/app/todolist/todolist.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-todolist',
   templateUrl: './todolist.component.html',
-  styleUrls: ['./todolist.component.scss']
+  styleUrls: ['./todolist.component.scss'],
 })
 export class TodolistComponent implements OnInit {
+  linkActive: string = 'link_active';
+  linkActive2: string = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  changeAStyle(): void {
+    this.linkActive = this.linkActive === '' ? 'link_active' : '';
+    this.linkActive2 = this.linkActive2 === 'link_active' ? '' : 'link_active';
   }
-
 }
